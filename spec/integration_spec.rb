3b0774adcd238616c4dc4b035c126b929a6a0298
Jennifer Kinsey <kinseyplusplus@gmail.com>
2017-05-05 14:30:00 -0700

    adds integration spec for going to a project page and seeing the content there.

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -13,35 +13,33 @@ describe('adding a new project', {:type => :feature}) do
   end
 end
 
-describe('viewing all of the projects', {:type => :feature}) do
+describe('viewing all of the projects on the homepage', {:type => :feature}) do
   it('allows a user to see all of the projects that have been created') do
-    project1 = Project.new({:project_name => 'blast the Digdogger in the eye', :id => 1})
+    project1 = Project.new({project_name: 'blast the Digdogger in the eye', id: 1})
     project1.save
     visit('/')
     expect(page).to have_content(project1.project_name)
   end
 end
 
-# describe('seeing details for a single list', {:type => :feature}) do
-#   it('allows a user to click a list to see the tasks and details for it') do
-#     test_list = List.new({:name => 'School stuff', :id => 2})
-#     test_list.save()
-#     test_task = Task.new({:description => "learn SQL", :list_id => test_list.id()})
-#     test_task.save()
-#     visit('/')
-#     click_link(test_list.name())
-#     expect(page).to have_content(test_task.description())
-#   end
-# end
+describe('seeing details for a single project', {:type => :feature}) do
+  it('allows a user to click on a project to see the volunteers and details for it') do
+    project1 = Project.new({project_name: 'Patra gets zapped with grappling hook', id: 1})
+    project1.save
+    visit('/')
+    click_link(project1.project_name)
+    expect(page).to have_content('There are no volunteers on this project')
+  end
+end
 #
 # describe('adding tasks to a list', {:type => :feature}) do
 #   it('allows a user to add a task to a list') do
 #     test_list = List.new({:name => 'School stuff', :id => 3})
 #     test_list.save()
-#     visit("/")
+#     visit('/')
 #     select('School stuff', :from => 'list_id')
-#     fill_in("Description", {:with => "learn SQL"})
-#     click_button("Add task")
-#     expect(page).to have_content("learn SQL")
+#     fill_in('Description', {:with => 'learn SQL'})
+#     click_button('Add task')
+#     expect(page).to have_content('learn SQL')
 #   end
 # end
