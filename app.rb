1fd9834c0712c773110e70a386354909fb97809a
Jennifer Kinsey <kinseyplusplus@gmail.com>
2017-05-05 11:43:44 -0700

    adds additional content to index.erb, allowing user to add a project and see it displayed. adds the appropriate routing to app.rb for this functionality.

--- app.rb
+++ app.rb
@@ -5,9 +5,17 @@ require('./lib/project')
 require('./lib/volunteer')
 require('pg')
 
-DB = PG.connect({:dbname => 'volunteer_tracker'})
+DB = PG.connect({dbname: 'volunteer_tracker'})
 
 get('/') do
+  @projects = Project.all
+  erb(:index)
+end
+
+post('/lists') do
+  project_name = params.fetch('project_name')
+  project1 = Project.new({project_name: project_name, id: nil})
+  project1.save
   @projects = Project.all
   erb(:index)
 end
