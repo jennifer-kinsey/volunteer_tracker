a446f4366346f55e4311fe812d532ee580e8537e
Jennifer Kinsey <kinseyplusplus@gmail.com>
2017-05-05 10:03:29 -0700

    adds the tests for the save and == methods on the Project class

--- lib/project.rb
+++ lib/project.rb
@@ -10,11 +10,22 @@ class Project
     returned_projects = DB.exec("SELECT * FROM projects;")
     projects = []
     returned_projects.each() do |project|
-      name = project.fetch("name")
-      project = project.fetch("id").to_i()
-      projects.push(Project.new({:project_name => project_name, :id => id}))
+      project_name = project.fetch("project_name")
+      id = project.fetch("id").to_i()
+      projects.push(Project.new({project_name: project_name, id: id}))
     end
     projects
   end
-  
+
+  def save
+    result = DB.exec("INSERT INTO projects (project_name) VALUES ('#{@project_name}') RETURNING id;")
+    @id = result.first.fetch('id').to_i
+  end
+
+  define_method(:==) do |another_project|
+    self.project_name.==(another_project.project_name).&(self.id().==(another_project.id()))
+  end
+
+
+
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -8,4 +8,20 @@ describe(Project) do
     end
   end
 
+  describe('#save') do
+    it('saves the projects to the database') do
+      project1 = Project.new({project_name: "take over Hyrule", id: nil})
+      project1.save
+      expect(Project.all).to eq([project1])
+    end
+  end
+
+  describe('#==') do
+    it("is the same project if the project name and id is the same") do
+      project1 = Project.new({project_name: "obtain master sword", id: nil})
+      project2 = Project.new({project_name: "obtain master sword", id: nil})
+      expect(project1).to eq(project2)
+    end
+  end
+
 end
